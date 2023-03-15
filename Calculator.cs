a4ce96b15371bf26569d1c9f85a5115a9ee47bcd
codhotgoreng <naafi@students.amikom.ac.id>
2023-03-15 12:08:16 +0700

    diisi dengan keterangan commit

--- Calculator.cs
+++ Calculator.cs
@@ -2,7 +2,24 @@ Class Calculator
 {
 	static void Main(string[] args)
 	{
+		int a = 10;
+		int b = 6;
+		
+		Console.Writeline("Hasil Penambahan: {0} + {1} = {2}", a, b, Penambahan(a, b))
+		Console.Writeline("Hasil Pengurangan: {0} - {1} = {2}", a, b, Pengurangan(a, b))
+		
+		Console.WriteLine("\nTekan sembarang key untuk keluar");
+		Console.ReadKey();
 	
+	}
+	static int Penambahan (int a, int b)
+	{
+		return a + b;
+	}
+	
+	static int Pengurangan (int a, int b)
+	{
+		return a - b;
 	}
 
 }
